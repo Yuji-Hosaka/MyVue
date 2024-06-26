0970f813ccb1739e760ca8c394cba2527f6229a4
Yuji Hosaka <h.kyouiku58@gmail.com>
2024-06-26 18:33:32 +0700

    How to add @click button

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 const title = 'Vue.JS Learning'
 let price = 9.99
+function increment() {
+  price += 1
+  console.log(price)
+}
 </script>
 
 <template>
   <h1>Title: {{ title }}</h1>
   <h2>Price: ${{ price - 1 }}</h2>
+  <button @click="increment">OK</button>
 </template>
 
 <style></style>
