49a8b52e71a3af14b7a83fcfe9d1431e5477eeca
Yuji Hosaka <h.kyouiku58@gmail.com>
2024-06-26 19:42:41 +0700

    How to create Reactived data and use ref()

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <script setup>
-const title = 'Vue.JS Learning'
-let price = 9.99
+import { ref } from 'vue'
+
+const title = ref('Vue.JS Learning')
+let price = ref(9.99)
 function increment() {
-  price += 1
-  console.log(price)
+  price.value += 1
 }
+
+const info = ref({
+  students: 1000,
+  rating: 4
+})
+console.log(info.value.students)
 </script>
 
 <template>
+  <!-- No need .value here, otherwise, it will be NaN -->
   <h1>Title: {{ title }}</h1>
   <h2>Price: ${{ price - 1 }}</h2>
   <button @click="increment">OK</button>
+  <h2>Student: {{ info.students }}</h2>
 </template>
 
 <style></style>
